4d748ec6fc1876a219ac105a2009422eb1088d53
Rajeeth18 <134668145+Rajeeth18@users.noreply.github.com>
2023-05-26 11:14:27 +0530

    Add files via upload

---
+++ myprogram2.py
@@ -0,0 +1,11 @@
+Python 3.10.5 (tags/v3.10.5:f377153, Jun  6 2022, 16:14:13) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+age=(13)
+print(age)
+13
+Weight=(56)
+print(Weight)
+56
+Height=133
+print(Height)
+133
